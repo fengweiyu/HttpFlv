bfae30d5b48cd2d884926649b14f07354021cc39
fengweiyu <984441750@qq.com>
2024-06-12 11:49:42 +0800

    [CHG]日志打印优化

--- HttpFlvServer/demo/main.cpp
+++ HttpFlvServer/demo/main.cpp
@@ -60,6 +60,6 @@ static void PrintUsage(char *i_strProcName)
 {
     printf("Usage: %s ServerPort \r\n",i_strProcName);
     printf("run default args: %s 9214 \r\n",i_strProcName);
-    printf("client input url eg: http://localhost:9214/file/H264AAC.flv\r\n",i_strProcName);
+    printf("client input url eg: http://localhost:9214/file/H264AAC.flv\r\n");
 }
 
