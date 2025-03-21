5ad77a0933a6e94cd8fd7ab6d315123c8b2de601
fengweiyu <984441750@qq.com>
2025-03-21 16:03:06 +0800

    [ADD]添加web播放器，支持http-flv/ws-flv等媒体流

---
+++ HttpFlvServer/html/player/ChunkConvert.js
@@ -0,0 +1,319 @@
+ 
+
+self.Module ={
+	onRuntimeInitialized : function () {
+		console.log('Wasm module initialized');
+	}
+	
+}
+
+var oConvert = null;
+
+class convert {
+    constructor() {
+        this.m_MediaSource = new MediaSource();
+        this.m_MediaElement = null;
+        this.m_SrcName = null;
+        this.m_SrcType = null;
+        this.m_DstName = null;
+        this.m_SrcBuffer = null;
+        this.m_SourceBuffer = null;
+        this.m_IsConverting = false;
+        this.m_IsPaused = false;
+        this.m_Queue = []; // 数据队列，用于存储尚未处理的数据块
+        this.m_CurrentOffset = 0;
+        oConvert = this;
+		
+		this.m_AccumulatedBuffer = new Uint8Array(0); // 累计缓冲区
+		this.m_AccumulatedSize = 0; // 累计大小
+        this.m_DownBuffer = new Uint8Array(0); // 累计缓冲区
+    }
+
+    /**
+     * 绑定媒体元素
+     * @param {HTMLMediaElement} mediaElement - 用于播放的媒体元素（如 <video>）
+     */
+    attachMediaElement(mediaElement) {
+        this.m_MediaElement = mediaElement;
+        this.m_MediaElement.src = URL.createObjectURL(this.m_MediaSource);
+        //this.m_MediaSource.addEventListener('sourceopen', this.MediaSourceOpen.bind(this));
+    }
+
+    /**
+     * 解绑媒体元素
+     */
+    detachMediaElement() {
+        if (this.m_MediaElement) {
+            this.m_MediaElement.src = '';
+            this.m_MediaElement = null;
+        }
+    }
+
+    /**
+     * 处理 MediaSource 打开事件
+     */
+    MediaSourceOpen() {
+        //this.m_SourceBuffer = this.m_MediaSource.addSourceBuffer('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+        //this.m_SourceBuffer.addEventListener('updateend', this.handleUpdateEnd.bind(this));
+        //this.processQueue(); // 开始处理队列中的数据
+    }
+
+    /**
+     * 处理 SourceBuffer 更新结束事件
+     */
+    handleUpdateEnd() {
+        if (this.m_Queue.length > 0 && !this.m_IsPaused) {
+            //this.processQueue(); // 继续处理队列中的数据
+        }
+    }
+
+    /**
+     * 外部接口：输入数据块
+     * @param {ArrayBuffer} dataChunk - 数据块
+     * @param {string} srcType - 源文件类型（如 ".webm"）
+     * @param {string} dstType - 目标文件类型（如 ".mp4"）
+     */
+    inputDataChunk(dataChunk, srcType, dstType) {
+		// 如果累计大小达到 1MB，触发下载操作
+		const uint8Chunk = new Uint8Array(dataChunk);
+		this.m_AccumulatedBuffer = this.concatBuffers(this.m_AccumulatedBuffer, uint8Chunk);
+		this.m_AccumulatedSize += uint8Chunk.length;
+		// console.log("AAAAAAAAAA",this.m_AccumulatedSize,1 * 1024 * 1024);
+		if (this.m_AccumulatedSize >= 256 * 1024) {
+		    //DownloadMedia(this.m_AccumulatedBuffer,'test.pri');
+		 	this.m_AccumulatedBuffer = new Uint8Array(0); // 清空累计缓冲区
+		 	this.m_AccumulatedSize = 0; // 重置累计大小
+		}
+		// console.clear();//清除日志
+		
+        if (!dataChunk || !srcType || !dstType) {
+            console.error('Invalid input data');
+            return;
+        }
+		
+        // 将数据块加入队列
+        this.m_Queue.push({ data: dataChunk, srcType, dstType });
+
+        // 如果没有在转换中，则开始处理队列
+        if (!this.m_IsConverting && !this.m_IsPaused) {
+            this.processQueue();
+        }
+    }
+	
+	/**
+	     * 合并两个 Uint8Array
+	     * @param {Uint8Array} buffer1 - 第一个缓冲区
+	     * @param {Uint8Array} buffer2 - 第二个缓冲区
+	     * @returns {Uint8Array} - 合并后的缓冲区
+	     */
+	concatBuffers(buffer1, buffer2) {
+		const result = new Uint8Array(buffer1.length + buffer2.length);
+		result.set(buffer1, 0);
+		result.set(buffer2, buffer1.length);
+		return result;
+	}
+	GetMediaDstEnc()
+	{
+		var bufDstMaxLen=10;
+		let bufDstVideoEnc = Module._malloc(bufDstMaxLen);
+		let bufDstAudioEnc = Module._malloc(bufDstMaxLen);
+		var ret = Module._GetEncodeType(bufDstVideoEnc,bufDstMaxLen,bufDstAudioEnc,bufDstMaxLen);
+		let VideoArray = new Uint8Array(bufDstMaxLen);
+		VideoArray.set(new Uint8Array(Module.HEAPU8.buffer, bufDstVideoEnc, 6)); // 将C中的数组拷贝到JS数组中
+		let AudioArray = new Int8Array(bufDstMaxLen);
+		AudioArray.set(new Uint8Array(Module.HEAP8.buffer, bufDstAudioEnc,6)); // 将C中的数组拷贝到JS数组中
+		const stringVideo = String.fromCharCode.apply(null, VideoArray);  
+		const stringAudio = String.fromCharCode.apply(null, AudioArray);  
+		Module._free(bufDstVideoEnc);
+		Module._free(bufDstAudioEnc);
+		return [stringVideo,stringAudio];
+	}
+    /**
+     * 处理队列中的数据块
+     */
+    processQueue() {
+        if (this.m_IsConverting || this.m_IsPaused || this.m_Queue.length === 0) {
+            return;
+        }
+
+        this.m_IsConverting = true;
+
+        const { data, srcType, dstType } = this.m_Queue.shift(); // 从队列中取出一个数据块
+        const convertedChunk = this.convert(data, srcType, dstType);
+
+        if (convertedChunk) {
+            if(!this.m_SourceBuffer)
+            {
+                var VideoCodec=null;
+                var AudioCodec=null;
+                let mMedia = this.GetMediaDstEnc();
+                let v = mMedia[0];
+                let a = mMedia[1];
+                if (v.includes("h264"))//video/webm; codecs="avc1.64001E"：用于 WebM 容器中的 H.264 视频（通常则使用 VP8 或 VP9）。
+                {//video/mp4; codecs="avc1.64001E"：用于 MP4 容器中的 H.264 视频。
+                    VideoCodec="avc1.42E01E";//avc1.42E01E
+                }//video/mp2t; codecs="avc1.42E01E"：用于 MPEG-TS 容器中的 H.264 视频
+                else if (v.includes("h265"))
+                {//video/mp4; codecs="hevc"：用于 MPEG-4 容器格式中的 H.265 视频。
+                    VideoCodec="hvc1.1.6.L93.B0";
+                }//用于传输流（如 TS 或 MPEG-TS）编码中的 H.265 视频。
+                if (a.includes("aac"))
+                {
+                    AudioCodec="mp4a.40.2";//audio/mp4; codecs="mp4a.40.2"：这是一般用于 MP4 容器中的 AAC 音频格式。
+                }//audio/aac：用于裸 AAC 数据。
+                else if(a.includes("g711a"))
+                {
+                    AudioCodec="alaw";//audio/mp4; codecs="law"：表示使用 G.711 A-law 编码的音频数据。
+                }//audio/g711：用于直接表示 G.711 编码的数据。
+                if(null != VideoCodec && null !=AudioCodec)
+                {
+                    this.m_SourceBuffer = this.m_MediaSource.addSourceBuffer('video/mp4; codecs="'+VideoCodec+','+AudioCodec+'"');
+                    this.m_MediaElement.style.display = 'block'; // 显示视频 = 'none'; // 隐藏视频  
+                }
+                else if(null != VideoCodec)
+                {
+                    this.m_SourceBuffer = this.m_MediaSource.addSourceBuffer('video/mp4; codecs="'+VideoCodec+'"');
+                    this.m_MediaElement.style.display = 'block'; // 显示视频 = 'none'; // 隐藏视频  
+                }
+                else if(null != AudioCodec)
+                {
+                    this.m_SourceBuffer = this.m_MediaSource.addSourceBuffer('video/mp4; codecs="'+AudioCodec+'"');
+                    this.m_MediaElement.style.display = 'block'; // 显示视频 = 'none'; // 隐藏视频  
+                }
+                else
+                {
+                    console.log('GetMediaDstEnc err v '+VideoCodec+' a '+AudioCodec);    
+                }
+            }
+            try {
+                this.m_SourceBuffer.appendBuffer(convertedChunk);
+                if( this.m_DownBuffer==null)
+                    this.m_DownBuffer=convertedChunk;
+                else
+                {
+                    //this.m_DownBuffer=this.appendTypedArray(this.m_DownBuffer,convertedChunk);
+                    if(this.m_DownBuffer.length>=256*1024)
+                    {
+                        //DownloadMedia(this.m_DownBuffer,"123123.mp4");
+                        //this.m_DownBuffer=null;
+                    }
+                }
+
+            } catch (error) {
+                console.error('Failed to append buffer:', error);
+                //this.m_Queue.unshift({ data, srcType, dstType }); // 如果失败，将数据块重新放回队列
+            }
+            //Module._Clean();
+            //this.convert(data, srcType, dstType);
+        } else {
+            //console.error('Failed to convert chunk');
+        }
+
+        this.m_IsConverting = false;
+
+        // 如果队列中还有数据，继续处理
+        if (this.m_Queue.length > 0 && !this.m_IsPaused) {
+            this.processQueue();
+        }
+    }
+    appendTypedArray(a, b) 
+    {  
+        // 创建一个新的 TypedArray，长度为 a 和 b 的长度之和  
+        const result = new (a.constructor)(a.length + b.length);  
+        
+        // 将 a 的内容复制到新数组中  
+        result.set(a, 0);  
+        
+        // 将 b 的内容复制到新数组中，从 a 的末尾开始  
+        result.set(b, a.length);  
+        
+        return result;  
+    } 
+    /**
+     * 转换数据块
+     * @param {ArrayBuffer} srcBuffer - 源数据
+     * @param {string} srcType - 源文件类型
+     * @param {string} dstType - 目标文件类型
+     * @returns {Uint8Array|null} - 转换后的数据块
+     */
+    convert(srcBuffer, srcType, dstType) {
+        if (!srcBuffer || srcBuffer.byteLength === 0) {
+            console.error('Invalid source buffer');
+            return null;
+        }
+
+        const arrDstType = intArrayFromString(dstType).concat(0);
+        const bufDstType = Module._malloc(arrDstType.length);
+        Module.HEAPU8.set(arrDstType, bufDstType);
+
+        const arrSrcType = intArrayFromString(srcType).concat(0);
+        const bufSrcType = Module._malloc(arrSrcType.length);
+        Module.HEAPU8.set(arrSrcType, bufSrcType);
+
+        const uint8View = new Uint8Array(srcBuffer);
+        const bufSrcFrame = Module._malloc(uint8View.byteLength);
+        Module.HEAPU8.set(uint8View, bufSrcFrame);
+
+        Module._InputData(bufSrcFrame, uint8View.byteLength, bufSrcType, bufDstType);
+
+        const bufDstMaxLen = uint8View.byteLength + (10 * 1024 * 1024);
+        const bufDstFrame = Module._malloc(bufDstMaxLen);
+        let ret = 0;
+        let len = 0;
+
+        do {
+            len += ret;
+            ret = Module._GetData(bufDstFrame + len, bufDstMaxLen - len);
+        } while (ret > 0);
+
+        const convertedChunk = new Uint8Array(len);
+        convertedChunk.set(new Uint8Array(Module.HEAPU8.buffer, bufDstFrame, len));
+
+        Module._free(bufDstType);
+        Module._free(bufSrcType);
+        Module._free(bufSrcFrame);
+        Module._free(bufDstFrame);
+
+        if (convertedChunk.length > 0) {
+            return convertedChunk;
+        } else {
+            //console.error('Conversion failed: empty result');
+            return null;
+        }
+    }
+
+    /**
+     * 暂停转换
+     */
+    pause() {
+        this.m_IsPaused = true;
+    }
+
+    /**
+     * 恢复转换
+     */
+    resume() {
+        this.m_IsPaused = false;
+        this.processQueue(); // 恢复时继续处理队列
+    }
+
+    /**
+     * 清空队列并停止转换
+     */
+    stop() {
+        this.m_Queue = [];
+        this.m_IsPaused = true;
+        this.m_IsConverting = false;
+        Module._Clean();
+    }
+
+    /**
+     * 销毁实例
+     */
+    destroy() {
+        this.stop();
+        this.detachMediaElement();
+        this.m_MediaSource = null;
+        this.m_SourceBuffer = null;
+    }
+}
---
+++ HttpFlvServer/html/player/player.js
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2020-2025 Hanson Yu All Rights Reserved.
+ *
+ * @author yu weifeng 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class player 
+{
+    constructor(url) 
+    {
+        this.m_Convert = new convert();
+        this.m_URL = url;
+        this.m_SrcType = ".pri";
+        this.m_DstType = ".mp4";
+    }
+
+    /**
+     * 绑定媒体元素
+     * @param {HTMLMediaElement} mediaElement - 用于播放的媒体元素（如 <video>）
+     */
+    attachMediaElement(mediaElement) 
+    {
+        this.m_Convert.attachMediaElement(mediaElement);
+    }
+    play() 
+    {
+        if(".flv"==this.m_URL.substring(this.m_URL.length - 4))
+        {
+            this.m_SrcType = ".flv";
+        }
+        else if(".dat"==this.m_URL.substring(this.m_URL.length - 4))
+        {
+            this.m_SrcType = ".pri";
+        }
+        else
+        {
+            console.error('unsupport url data : ', this.m_URL); 
+            return;            
+        }
+        
+        if("http"==this.m_URL.substring(0,4) || "https"==this.m_URL.substring(0,5))
+        {
+            this.HandleHttpReq(this.m_URL);
+        }
+        else if("ws"==this.m_URL.substring(0,2) || "wss"==this.m_URL.substring(0,3))
+        {
+            this.HandleWsURL(this.m_URL);
+        }
+        else
+        {
+            console.error('unsupport url : ', this.m_URL);  
+        }
+    }
+    
+    
+    detachMediaElement() 
+    {
+        this.m_Convert.detachMediaElement()
+    }
+    destroy() 
+    {
+        this.m_Convert.destroy()
+    }
+    async HandleHttpReq(url) 
+    {  
+        try 
+        {  
+            const response = await fetch(url);  
+            if (!response.ok) 
+            {  
+                throw new Error(`HTTP error! status: ${response.status}`);  
+            }  
+
+            // 创建一个读取流  
+            const reader = response.body.getReader();  
+            let receivedLength = 0; // 收到的总字节数  
+            while (true) 
+            {  
+                const { done, value } = await reader.read();  
+                if (done) 
+                {  
+                    console.log('读取完成:', receivedLength);  
+                    break; // 读取完成  
+                }  
+                receivedLength += value.byteLength; // 更新总字节数  
+                this.m_Convert.inputDataChunk(value, this.m_SrcType, this.m_DstType); 
+            }  
+        } 
+        catch (error) 
+        {  
+            console.error('获取 FLV 数据时出错:', error);  
+        }  
+    }
+    HandleHttpURL(url) 
+    {  
+        const xhr = new XMLHttpRequest();  
+        // 初始化请求  
+        xhr.open('POST', url, true);  
+    
+        // 设置响应类型为 'text' 或 'arraybuffer'（取决于数据格式）  
+        xhr.responseType = 'arraybuffer'; // 可以根据实际数据类型选择  
+
+        xhr.onload = function () 
+        {  
+            if (xhr.status >= 200 && xhr.status < 300) 
+            {  
+                console.log('Response received:', xhr.response);  
+            } 
+            else 
+            {  
+                console.error('Request failed with status:', xhr.status);  
+            }  
+        };  
+
+        // 处理进度更新  
+        xhr.onprogress = this.HandleHttpData.bind(this);  
+
+        xhr.onerror = function () 
+        {  
+            console.error('Request error');  
+        };  
+  
+    } 
+    HandleHttpData(event) 
+    {
+        if (event.lengthComputable) 
+        {  
+            const loaded = event.loaded;  
+            const total = event.total;  
+            console.log(`Loaded ${loaded} of ${total} bytes`);  
+        } 
+        else 
+        {  
+            // 数据流信息（未计算总字节时）  
+            console.log(`Loaded ${event.loaded} bytes`);  
+        }  
+
+        // 这里可以处理实时接收到的数据  
+        const chunk = xhr.response; // 或者 xhr.response（根据 responseType）  
+        // 输入数据块进行转换
+        this.m_Convert.inputDataChunk(chunk, this.m_SrcType, this.m_DstType);  
+    }
+    HandleWsURL(url) 
+    {  
+        // 创建 WebSocket 连接  
+        const socket = new WebSocket(url);  
+
+        // 连接成功时的回调  
+        socket.onopen = function () 
+        {  
+            console.log('WebSocket connection opened.');  
+            // 你可以在这里发送初始消息  
+            // socket.send('Hello Server!');  
+        };  
+
+        //接收到消息时的回调  
+        socket.onmessage = this.HandleWsBlob.bind(this);
+
+        // 连接关闭时的回调  
+        socket.onclose = function () 
+        {  
+            console.log('WebSocket connection closed.');  
+        };  
+
+        // 处理错误的回调  
+        socket.onerror = function (error) 
+        {  
+            console.error('WebSocket Error:', error);  
+        }; 
+  
+    } 
+    HandleWsBlob(event) 
+    {
+        const blob = event.data; // 获取接收到的 Blob 数据  
+        // 使用 FileReader 将 Blob 转换为 ArrayBuffer  
+        const reader = new FileReader();   
+        reader.onload = this.HandleWsData.bind(this);
+        reader.readAsArrayBuffer(blob); // 开始转换  
+    }
+    HandleWsData(event) 
+    {
+        // 这里可以处理实时接收到的数据  
+        const arrayBuffer = event.target.result; // 获取 ArrayBuffer   
+        // 输入数据块进行转换
+        this.m_Convert.inputDataChunk(arrayBuffer, this.m_SrcType, this.m_DstType);  
+    }
+}
+
+
+
+
+
+
+
+// factory method
+function createPlayer(mediaDataSource) 
+{
+    let mds = mediaDataSource;
+    if (mds == null || typeof mds !== 'object') {
+        console.log('MediaDataSource must be an javascript object!');
+    }
+
+    if (!mds.hasOwnProperty('url')) {
+        console.log('MediaDataSource must has url field to indicate video file url!');
+    }
+
+    return new player(mds.url);
+}
+
+// interfaces
+let playerjs = {};
+
+playerjs.createPlayer = createPlayer;
+
+//export default playerjs;
